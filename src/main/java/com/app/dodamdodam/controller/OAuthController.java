ad6f1cac33aa0d72144d0e2b6afc94d8cfa29336
dev-mwYoon <dev.mwYoon@gmail.com>
2023-05-30 21:13:03 +0900

    컨트롤러 변경점

--- src/main/java/com/app/dodamdodam/controller/OAuthController.java
+++ src/main/java/com/app/dodamdodam/controller/OAuthController.java
@@ -27,56 +27,32 @@ public class OAuthController {
     @GetMapping("/")
     public RedirectView oAuthLogin(HttpSession session, RedirectAttributes redirectAttributes){
         log.info(" ------------------- 로그인 처리 후 맨 마지막 컨트롤러 ------------------------------------- ");
-        MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
-        Long memberId = (Long) session.getAttribute("memberId");
         log.info("==================================================");
-        log.info(memberDTO + "");
-        log.info(memberId + "");
 
-
-        if (memberDTO != null && memberDTO.getMemberId() == null) {
-            redirectAttributes.addFlashAttribute("member", memberDTO);
-            return new RedirectView("/member/join");
+        if (session.getAttribute("member") != null) {
+            MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
+            log.info(memberDTO + "");
+            if (memberDTO.getMemberId() == null) {
+                redirectAttributes.addFlashAttribute("member", memberDTO);
+                return new RedirectView("/member/join");
+            }
         }
 
-        MemberDTO memberInfo = memberService.getMemberInfo(memberId);
+        if (session.getAttribute("memberId") != null){
+            Long memberId = (Long) session.getAttribute("memberId");
+            log.info(memberId + "");
 
-        if (memberId != null && memberInfo.getMemberRole() == Role.ADMIN){
-            return new RedirectView("/admins/home");
-        } else if (memberId != null && memberInfo.getMemberRole() == Role.MEMBER){
-            return new RedirectView("/home");
-        } else {
-            return new RedirectView("/member/logout");
+            MemberDTO memberInfo = memberService.getMemberInfo(memberId);
+
+            if (memberInfo.getMemberRole() == Role.ADMIN){
+                return new RedirectView("/admins/home");
+            } else {
+                return new RedirectView("/home");
+            }
         }
+
+        return new RedirectView("/home");
+
     }
-
-
-//    @GetMapping("")
-//    public void main(@AuthenticationPrincipal UserDetail userDetail, Model model) {
-//        Long memberId = null;
-//        if (session.getAttribute("member") == null) {
-//            if(userDetail != null){
-//                Member member = memberService.getOptionalMember(userDetail.getId()).orElseGet(null);
-//                MemberDTO memberDTO = memberService.toMemberDTO(member);
-//                session.setAttribute("member", memberDTO);
-//            }
-//        } else {
-//            memberId = ((MemberDTO)session.getAttribute("member")).getId();
-//            MemberDTO memberDTO = memberService.getMember(memberId);
-//            session.setAttribute("member", memberDTO);
-//        }
-//
-//        List<GroupChallengeDTO> groupChallengeDTOS = groupChallengeService.getGroupChallengeList(PageRequest.of(0, 6)).getContent();
-//        List<GroupCalendarDTO> calendarDTOS = groupChallengeService.findAllCalendar();
-//        MemberDTO memberInfo = null;
-//        if (userDetail != null) {
-//            memberInfo = myPageService.getMemberDTO(userDetail.getId());
-//        } else if (memberId != null){
-//            memberInfo = myPageService.getMemberDTO(memberId);
-//        }
-//        model.addAttribute("memberDTO", memberInfo);
-//        model.addAttribute("groupChallengeDTOS", groupChallengeDTOS);
-//        model.addAttribute("calendarDTOS", calendarDTOS);
-//    }
 
 }
